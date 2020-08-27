6a4ce467417d56789e0bfa13d24d15fab39d6a17
Alessandro <alessandro.boscato@gmail.com>
2020-08-27 15:49:13 +0200

    1° tentativo (errore)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-// Palidroma
-// Chiedere all’utente di inserire una parola
+
+//1. Chiedere all’utente di inserire una parola
+var parola = prompt("Inserisci una parola e ti diremo se è palindroma");
+
 // Creare una funzione per capire se la parola inserita è palindroma
+var condizione = false;
+function palindromo(letterA, letterZ) {
+   if (letterA == letterZ) {
+     condizione = true;
+   }
+}
+
+for (var i = 0; i <= parola.length / 2; i++ ) {
+  var corrispondenza = palindromo(parola[i], parola[parola.length-i])
+}
+
+console.log(condizione);
+
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
